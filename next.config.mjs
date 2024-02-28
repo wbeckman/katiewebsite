a9f5e249363a7942450830c47123ac80546f7dc5
Will Beckman <wbeckman9@gmail.com>
2024-02-28 13:44:15 -0500

    oops

--- next.config.mjs
+++ next.config.mjs
@@ -5,24 +5,24 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  // output: "export",
+  output: "export",
 
-  // /**
-  //  * Set base path. This is the slug of your GitHub repository.
-  //  *
-  //  * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-  //  */
-  // basePath: "/katiewebsite",
-  // assetPrefix: "/katiewebsite",
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/katiewebsite",
+  assetPrefix: "/katiewebsite",
 
-  // /**
-  //  * Disable server-based image optimization. Next.js does not support
-  //  * dynamic features with static exports.
-  //  *
-  //  * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-  //  */
-  // images: {
-  //   unoptimized: true,
-  // },
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 export default nextConfig;
